7e9643c024f26a793eb88893fb655f237e20595e
Daniel Bright <brahyt@gmail.com>
2019-12-30 19:27:39 -0800

    added fetch for dash

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import TokenService from '../../services/token-service'
+import config from '../../config'
 
 class DashboardRoute extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      language: {
+        head: '',
+        id: '',
+        name: '',
+        total_score: '',
+        user_id: ''
+      },
+      words: []
+
+    }
+  }
+  async componentDidMount(){
+     const response = await fetch(`${config.API_ENDPOINT}/language`, {
+       method: 'GET',
+       headers: {
+         "Authorization": `Bearer ${TokenService.getAuthToken()}`
+       }
+    })
+    const {language, words} = await response.json()
+    await this.setState({
+      language,
+      words
+    })
+  }
+
   render() {
     return (
       <section>
-        implement and style me
+        DASHBOARDimplement and style me
       </section>
     );
   }
