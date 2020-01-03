02125d60641b780a920e63362bf99de79d594da9
Daniel Bright <brahyt@gmail.com>
2020-01-03 11:18:24 -0800

    stuff

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,5 +1,5 @@
-import config from '../config'
-import TokenService from './token-service'
+import config from '../config';
+import TokenService from './token-service';
 
 const AuthApiService = {
   postUser(user) {
@@ -9,40 +9,31 @@ const AuthApiService = {
         'content-type': 'application/json',
       },
       body: JSON.stringify(user),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json(),
+    );
   },
-  postLogin({ username, password }) {
+  postLogin({username, password}) {
     return fetch(`${config.API_ENDPOINT}/auth/token`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
       },
-      body: JSON.stringify({ username, password }),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(err => Promise.reject(err))
-          : res.json()
-      )
+      body: JSON.stringify({username, password}),
+    }).then(res =>
+      !res.ok ? res.json().then(err => Promise.reject(err)) : res.json(),
+    );
   },
   refreshToken() {
     return fetch(`${config.API_ENDPOINT}/auth/token`, {
       method: 'PUT',
       headers: {
-        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
       },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json(),
+    );
   },
-}
+};
 
-export default AuthApiService
+export default AuthApiService;
