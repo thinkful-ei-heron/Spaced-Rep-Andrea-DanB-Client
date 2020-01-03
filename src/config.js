9428fad45bae7a4d3a1cb3b61465e09c3e7dc57b
Daniel Bright <brahyt@gmail.com>
2020-01-03 08:49:23 -0800

    server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://git.heroku.com/spaced-repetition-dan-andrea.git/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
